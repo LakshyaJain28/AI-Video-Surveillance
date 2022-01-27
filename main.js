82cb100e2dc40836c0745baba152c852a86276c5
LakshyaJain28 <79844523+LakshyaJain28@users.noreply.github.com>
2022-01-27 16:45:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+video="";
+status="";
+
+function preLoad(){
+    video=createVideo("video.mp4");
+    video.hide();
+}
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("model loaded !");
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+function draw()
+{
+    image(video,0,0,480,380);
+}
